c99b2af207a2d8973e5e3693c5cef40783bc5eb1
Shivprasad_Suryakar <shivprasadsuryakar@gmail.com>
2026-07-30 12:03:35 +0530

    Change app.run to bind to all interfaces

--- main.py
+++ main.py
@@ -82,4 +82,4 @@ if __name__ == '__main__':
         webbrowser.open_new("http://127.0.0.1:5000/")
     
     Timer(2.0, open_browser).start() 
-    app.run(debug=True, port=5000)
+    app.run(host='0.0.0.0', port=5000, debug=True)
